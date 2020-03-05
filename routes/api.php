08655506b7c6a4d6985e98d075aff4660db22fce
Yassine Ammar <yassine@whispli.com>
2020-03-05 21:35:38 +1100

    chore: rename route

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::get('users', 'UserController@index')
         ->name('get.users');
 
-    Route::get('marioLaps', 'MarioLapController@index')
+    Route::get('mario-laps', 'MarioLapController@index')
         ->name('get.active_mario_laps');
 
     Route::post('rounds', 'RoundController@store')
